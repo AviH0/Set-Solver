fa120daf01c071c65cbc01ee736e6192bf6dfd8f
AviH0 <45289949+AviH0@users.noreply.github.com>
2022-07-20 19:02:48 +0300

    Some changes in tests

--- app/src/test/java/com/avih/set_solver/ImageProcTest.java
+++ app/src/test/java/com/avih/set_solver/ImageProcTest.java
@@ -23,6 +23,7 @@ import static com.avih.set_solver.image_processing.ImageProcessor.unWarpCard;
 import static org.bytedeco.opencv.global.opencv_highgui.imshow;
 import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
 import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
+import static org.bytedeco.opencv.global.opencv_imgproc.contourArea;
 
 import android.content.Context;
 
@@ -38,7 +39,10 @@ public class ImageProcTest {
     @Test
     public void testExtractCard_1()
     {
-        Mat src = imread(TEST_IMAGE_PATH_1);
+        Context c = RuntimeEnvironment.getApplication();
+        ImageProcessor imageProcessor = new ImageProcessor(c, R.raw.diamond, R.raw.round, R.raw.squiggle);
+        int IMAGE_NUM = 1;
+        Mat src = imread(TEST_IMAGE_PATH_BASE + IMAGE_NUM +  ".jpg");
         // Check if everything was fine
         if (src.data().isNull())
             return;
@@ -46,8 +50,11 @@ public class ImageProcTest {
         MatVector cardContours = getCardContours(src);
         for (int i = 0; i < cardContours.size(); i++)
         {
-            Mat card = unWarpCard(src, cardContours, i);
-            imwrite(TEST_IMAGE_PATH_1 + ".card" + i + ".jpg", card);
+            boolean isCardValue = imageProcessor.isCard(cardContours.get(i));
+            if (isCardValue) {
+                Mat card = unWarpCard(src, cardContours, i);
+                imwrite(OUTPUT_PATH + "img" + IMAGE_NUM + "_card_" + i + ".jpg", card);
+            }
         }
     }
 
@@ -64,7 +71,7 @@ public class ImageProcTest {
     {
         Context c = RuntimeEnvironment.getApplication();
         ImageProcessor imageProcessor = new ImageProcessor(c, R.raw.diamond, R.raw.round, R.raw.squiggle);
-        for (int x = 0; x < 5; x++) {
+        for (int x = 0; x < 25; x++) {
             String image_path = TEST_IMAGE_PATH_BASE + (x+1) + ".jpg";
             Mat src = imread(image_path);
             // Check if everything was fine
@@ -73,11 +80,13 @@ public class ImageProcTest {
             // Show source image
             MatVector cardContours = getCardContours(src);
             for (int i = 0; i < cardContours.size(); i++) {
-                Mat card = unWarpCard(src, cardContours, i);
-                boolean isCardValue = imageProcessor.isCard(card);
-//                System.out.println("img " + (x+1) + ", card " + i + " card value: " + isCardValue);
+                boolean isCardValue = imageProcessor.isCard(cardContours.get(i));
+                if (isCardValue) {
+                    Mat card = unWarpCard(src, cardContours, i);
+//                    System.out.println("img " + (x + 1) + ", card " + i + " card value: " + isCardValue);
 //                if (isCardValue)
                 imwrite(OUTPUT_PATH + "img" + (x+1) + "_card_" + i + ".jpg", card);
+                }
             }
         }
     }
@@ -88,17 +97,22 @@ public class ImageProcTest {
     public void testExtractCard_2()
     {
 
-        Mat src = imread(TEST_IMAGE_PATH_2);
+        Context c = RuntimeEnvironment.getApplication();
+        ImageProcessor imageProcessor = new ImageProcessor(c, R.raw.diamond, R.raw.round, R.raw.squiggle);
+        int IMAGE_NUM = 27;
+        Mat src = imread(TEST_IMAGE_PATH_BASE + IMAGE_NUM +  ".jpg");
         // Check if everything was fine
         if (src.data().isNull())
             return;
         // Show source image
         MatVector cardContours = getCardContours(src);
-
         for (int i = 0; i < cardContours.size(); i++)
         {
-            Mat card = unWarpCard(src, cardContours, i);
-            imwrite(TEST_IMAGE_PATH_2 + ".card" + i + ".jpg",  card);
+            boolean isCardValue = imageProcessor.isCard(cardContours.get(i));
+            if (isCardValue) {
+                Mat card = unWarpCard(src, cardContours, i);
+                imwrite(OUTPUT_PATH + "img" + IMAGE_NUM + "_card_" + i + ".jpg", card);
+            }
         }
     }
     @Test
